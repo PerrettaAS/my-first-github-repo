108c2cef5f3c4a1b870321debe7f4ada7e54ab43
PerrettaAS <scottaperretta@gmail.com>
2015-12-24 18:15:58 -0500

    Add portfolio content.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-$( document ).ready(function() {
-	$(".content-block").hide().each(function(i) {
-		$(this).delay(i * 400).fadeIn(800);
+$(document).ready(function() {
+	$(".portfolio-content").hide();
+	$(".home-content .content-block").hide().each(function(i) {
+		$(this).delay(i * 250).fadeIn(800);
+	});
+});
+
+currentActivePage = "home";
+
+$("#home, #portfolio, #resume, #contact").click(function(event) {
+	$("." + currentActivePage + "-content").fadeOut(500, function() {
+		var clicked = event.target.id;
+		$("#" + currentActivePage + "").removeClass("active");
+		$("." + clicked + "-content").fadeIn(0);
+		$("." + clicked + "-content .content-block").hide().each(function(i) {
+			$(this).delay(i * 250).fadeIn(800);
+		});
+		var activePage = document.getElementById(clicked);
+		console.log(activePage.id);
+		activePage.className = activePage.className + " active";
+		currentActivePage = clicked;
 	});
 });
