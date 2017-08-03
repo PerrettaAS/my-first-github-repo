21e93eaeebdbb9aa597e26ad9c421fd96c9f09cf
PerrettaAS <scottaperretta@gmail.com>
2017-08-03 02:31:15 -0400

    Add responsiveness and content to resume and portfolio tabs.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$("#nav-about, #nav-resume, #nav-portfolio").click(clickEvent);
+	$("#nav-about-mobile, #nav-resume-mobile, #nav-portfolio-mobile").click(clickEvent);
     document.getElementById("content-portfolio").style.display = "none";
     document.getElementById("content-resume").style.display = "none";
 });
@@ -14,7 +15,9 @@ function clickEvent(event) {
         setTimeout(function() { active = false; }, 1000)
         var currentTarget = event.currentTarget.id.split("-")[1];
         document.getElementById("nav-" + currentlySelected).style.textDecoration = "none";
+        document.getElementById("nav-" + currentlySelected + "-mobile").style.textDecoration = "none";
         document.getElementById("nav-" + currentTarget).style.textDecoration = "underline";
+        document.getElementById("nav-" + currentTarget + "-mobile").style.textDecoration = "underline";
         $("#content-" + currentlySelected).fadeOut("slow", function() {
             $("#content-" + currentTarget).fadeIn("slow", function() {
                 document.getElementById("content-" + currentTarget).style.display = "inline-block";
